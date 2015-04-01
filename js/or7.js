682860bca0eae6ab19e0f676840cf0cb9d366433
laurenjrichie <laurenjrichie@gmail.com>
2015-04-01 16:27:54 -0700

    new or7 coordinates, info nums showing on hover

--- js/or7.js
+++ js/or7.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
   startPlaying();
 });
 
-var map;
+var map,
+    playInterval = null,
+    index = 0;
 
 function initialize() {
   var mapOptions = {
@@ -10,16 +12,16 @@ function initialize() {
     zoom: 7,
     mapTypeId: google.maps.MapTypeId.HYBRID
   };
-
   map = new google.maps.Map(document.getElementById('or7-map-canvas'), mapOptions);
-  
 }
-
-var playInterval = null;
 
 function startPlaying() {
   $('#play-or7').on('click', function() {
-    getData();
+    if (playInterval != null) {
+      stopPlaying();
+    } else {
+      getData();
+    }
   });
 }
 
@@ -34,26 +36,90 @@ function getData() {
     for (var i = 0; i < rows.length; i++) {
       data.push([rows[i].xcoord, rows[i].ycoord]);
     }
-    plotCoordinates(data); // how to have this function return data and then call plotCoordinates from startPlaying??
+    plotCoordinates(data, index); // how to have this function return data and then call plotCoordinates from startPlaying??
   });
 }
 
-function plotCoordinates(data) {
-  var i = 0;
+var infoLatLongs = {
+  one: [45.728322, -117.610243],
+  two: [45.474668, -118.016737],
+  three: [45.095913, -117.698133],
+  four: [42.850894, -121.587294],
+  five: [42.268259, -120.873182],
+  six: [42.552164, -122.916639],
+  seven: [41.598083, -123.378065]
+}
+
+function plotCoordinates(data, index) {
   playInterval = setInterval(function() {
     var image = 'img/paw.png';
-    if (i == 103) {
-      clearInterval(playInterval);
-      i = 0;
+    if (index == 42) {
+      stopPlaying();
+      index = 0;
+    } else {
+      plotInfoNums(index);
     }
     var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(data[i][0], data[i][1]),
+      position: new google.maps.LatLng(data[index][0], data[index][1]),
       map: map,
       icon: image,
     });
-    i++;
-  }, 150);
+    index++;
+    console.log(index);
+  }, 300);
 }
 
+function plotInfoNums(index) {
+  if (index == 1) {
+    showInfo("one", infoLatLongs);
+  } else if (index == 3) {
+    showInfo("two", infoLatLongs);
+  } else if (index == 5) {
+    showInfo("three", infoLatLongs);
+  } else if (index == 18) {
+    showInfo("four", infoLatLongs);
+  } else if (index == 27) {
+    showInfo("five", infoLatLongs);
+  } else if (index == 40) {
+    showInfo("six", infoLatLongs);
+  } else if (index == 41) {
+    showInfo("seven", infoLatLongs);
+  }
+}
+
+function stopPlaying() {
+  clearInterval(playInterval);
+  playInterval = null;
+}
+
+function showInfo(num, infoLatLongs) {
+  var latLongs = infoLatLongs[num],
+      image = 'img/' + num + '.ico',
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(latLongs[0], latLongs[1]),
+        map: map,
+        icon: image,
+      });
+  showHoverInfo(marker, num);
+}
+
+function generateHtml(num) {
+  var source = $("#or7-info-" + num).html(),
+      template = Handlebars.compile(source),
+      html = template();
+  return html;
+}
+
+function showHoverInfo(marker, num) {
+  var infowindow = new google.maps.InfoWindow({
+      content: generateHtml(num),
+  });
+  google.maps.event.addListener(marker, 'mouseover', function() {
+    infowindow.open(map,marker);
+  });
+  google.maps.event.addListener(marker, 'mouseout', function() {
+    infowindow.close(map,marker);
+  });
+}
 
 google.maps.event.addDomListener(window, 'load', initialize);
