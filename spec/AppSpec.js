e2e2f422757b6d3879434b4c2774a63d16f20c73
laurenjrichie <laurenjrichie@gmail.com>
2015-04-17 11:06:26 -0700

    refactor tests & appends path element to map

--- spec/AppSpec.js
+++ spec/AppSpec.js
@@ -1,6 +1,8 @@
 describe('PopMap', function() {
   it('loads the json for the US map', function() {
-    var popMap = new PopMap();
+    var width = '100';
+    var height = '100';
+    var popMap = new PopMap(width, height, 'body');
     
     var jsonSpy = spyOn(d3, 'json');
     popMap.loadData();
@@ -9,45 +11,53 @@ describe('PopMap', function() {
     
     expect(firstCall.args[0]).toEqual('data/us.json');
     var callback = firstCall.args[1];
-    callback(null, { some: 'object'});
+    callback(null, { objects: { land: { some: 'topo stuff' }}});
 
-    expect(popMap.usJson).toEqual({ some: 'object' });
+    expect(popMap.usJson).toEqual({ objects: { land: { some: 'topo stuff' }}});
   });
   
   it('shows a us map', function(done) {
-    var popMap = new PopMap();
-
+    var width = '100';
+    var height = '100';
+    var popMap = new PopMap(width, height, 'body');
+    
     popMap.loadData(function() {
-      var width = '100';
-      var height = '100';
-      var container = 'body'
-      popMap.drawMap(width, height, container);
+      var container = 'body';
+      popMap.drawMap(container);
     
       var svg = $('body svg');
       expect(svg).toExist();
       expect(svg.attr('width')).toEqual('100');
       expect(svg.attr('height')).toEqual('100');
-      
       done();
     });
   });
   
   it('appends the map to the correct div', function(done) {
-    var popMap = new PopMap();
-
+    var width = '100';
+    var height = '100';
+    var popMap = new PopMap(width, height, '.map');
+    
     popMap.loadData(function() {  
-      var width = '200';
-      var height = '200';
-      var container = '.map';
-      
-      popMap.drawMap(width, height, container);
       var map = $('.map svg');
       expect(map).toExist();
-      expect(map.attr('width')).toEqual('200');
-      expect(map.attr('height')).toEqual('200');
+      expect(map.attr('width')).toEqual('100');
+      expect(map.attr('height')).toEqual('100');
       
       done();
     });
   });
+  
+  it('appends a path element to the map', function(done) {
+    var width = '100';
+    var height = '100';
+    var popMap = new PopMap(width, height, '.map');
     
+    popMap.loadData(function() {
+      var path = $('.map path.land');
+      expect(path).toExist();
+      expect(path.attr('d')).not.toBe(undefined);
+      done();
+    });
+  });  
 });
--- src/app.js
+++ src/app.js
@@ -6,28 +6,58 @@ function getHeight() {
   return getWidth()/2.048;
 }
 
-function PopMap(width, height) {
+function PopMap(width, height, container) {
   this.width = width;
   this.height = height;
+  this.container = container;
+}
+
+PopMap.prototype.setupProjection = function() {
+  this.projection = d3
+    .geo.albers()
+    .scale(this.width)
+    .translate([getWidth()/2, getHeight()/2]);
+}
+
+PopMap.prototype.setupPath = function() {
+  this.path = d3
+    .geo.path()
+    .projection(this.projection)
+    .pointRadius(1.5);
 }
 
 PopMap.prototype.loadData = function(done) {
   queue()
     .defer(d3.json, 'data/us.json')
     .await(function() {
-      this.loaded.apply(arguments);
+      this.loaded.apply(this, arguments);
       if(done) { done(); }
     }.bind(this));
 }
 
 PopMap.prototype.loaded = function(err, usJson) {
   this.usJson = usJson;
+  this.drawMap();
 }
 
-PopMap.prototype.drawMap = function(width, height, container) {
+PopMap.prototype.drawMap = function() {
   this.svg = d3
-    .select(container)
+    .select(this.container)
     .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    .attr("width", this.width)
+    .attr("height", this.height);
+
+  this.setupProjection();
+  this.setupPath();
+  
+  this.svg
+    .append("path")
+    .attr("class", "land")
+    .datum(topojson.feature(this.usJson, this.usJson.objects.land))
+    .attr("d", this.path);
 }
+
+// hand roll part of the data, let code guide how to structure it. 
+// determine ideal struture then produce ruby code that takes csv and creates json in that structure
+// continue writing jasmine tests until we know what the data should look like
+// this is working outside in
